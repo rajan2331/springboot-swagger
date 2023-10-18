9b410de916fb97a80dee549a86ab58efb6e7d902
LMN.RAJAN <rajansharma2331@gmail.com>
2023-10-18 14:13:31 +0400

    intial commit

---
+++ src/main/java/com/java/swagger/contoller/HelloController.java
@@ -0,0 +1,14 @@
+package com.java.swagger.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/api")
+	public String sayHello() {
+		return "Swagger Hello World";
+	}
+}
